b345e88bff8edf646986d8e7c18c8409954e3924
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-05-15 18:06:52 -0500

    refactor(html): Applying styles for subheader color

--- components/ofer-sidebar.vue
+++ components/ofer-sidebar.vue
@@ -1,8 +1,8 @@
 <template>
   <v-sidebar v-model="$store.state[opened]" drawer height="100%" fixed>
-    <v-subheader v-if="title" v-text="title" />
-    <v-divider v-if="title"/>
     <v-list dense>
+      <v-subheader class="white--text" v-if="title" v-text="title" />
+      <v-divider v-if="title"/>
       <v-list-item v-for="(item,i) in items" :key="i">
         <v-list-tile is="nuxt-link" :to="path + '/' + item._id" class="list__tile">
           <v-list-tile-content>
