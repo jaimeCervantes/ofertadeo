5ad3c42a7eb47da7b4d593ce23d443f21c484b77
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-05-16 12:05:09 -0500

    refactor(html,css): Changing how to display a promotion

--- pages/promociones/_id.vue
+++ pages/promociones/_id.vue
@@ -7,42 +7,13 @@
     <ofer-content :breadcrumbs="breadcrumbs">
       <template slot="items">
         <v-col class="pa-3" xs12 sm12 md9 lg9 xl9 v-for="(item,i) in catalogs" :key="i">
-          <v-card raised>
-            <v-card-row class="light-blue">
-              <v-card-title>
-                <span class="white--text">{{item.title}}</span>
-                <v-spacer></v-spacer>
-                <v-menu id="space" bottom left origin="top right" transition="v-scale-transition">
-                  <v-btn icon="icon" slot="activator" class="white--text">
-                    <v-icon>more_vert</v-icon>
-                  </v-btn>
-                  <v-list>
-                    <v-list-item>
-                      <v-list-tile>
-                        <v-list-tile-title>Acccion 1</v-list-tile-title>
-                      </v-list-tile>
-                    </v-list-item>
-                    <v-list-item>
-                      <v-list-tile>
-                        <v-list-tile-title>Accion 2</v-list-tile-title>
-                      </v-list-tile>
-                    </v-list-item>
-                  </v-list>
-                </v-menu>
-              </v-card-title>
-            </v-card-row>
-            <v-card-row :img="item.thumbnail" height="300px"></v-card-row>
-            <v-card-text>
-              <div v-html="item.content"></div>
-            </v-card-text>
-            <v-card-row actions class="light-blue mt-0">
-              <v-btn flat class="white--text">Accion 1</v-btn>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon class="white--text">explore</v-icon>
-              </v-btn>
-            </v-card-row>
-          </v-card>
+          <div class="promotion">
+            <h1 class="display-1">{{item.title}}</h1>
+            <div class="thumbnail">
+              <img :src="item.thumbnail" class="mr-3" alt="">
+            </div>
+            <div class="promotion-content" v-html="item.content"></div>
+          </div>
         </v-col>
       </template>
     </ofer-content>
