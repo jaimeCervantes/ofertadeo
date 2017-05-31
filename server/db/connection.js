f6a867e5788215a0ec364f80849e9a0e0c02cc5b
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-05-31 10:30:50 -0500

    fixing tabs

--- server/db/connection.js
+++ server/db/connection.js
@@ -4,9 +4,9 @@ var MongoClient = require('mongodb').MongoClient;
  
 function getConnection(config) {
   //connPromise is pending when trying to connect to mongodb atlas
-// var shards = config.db.shard1 + ',' + config.db.shard2 + ',' + config.db.shard3;
-// var url = 'mongodb://' + config.db.user + ':' + config.db.password + '@' + shards + '/' + config.db.name + config.db.queryString;
-var url = 'mongodb://' + config.db.user + ':' + config.db.password + '@' + config.db.host + '/' + config.db.name;
+  // var shards = config.db.shard1 + ',' + config.db.shard2 + ',' + config.db.shard3;
+  // var url = 'mongodb://' + config.db.user + ':' + config.db.password + '@' + shards + '/' + config.db.name + config.db.queryString;
+  var url = 'mongodb://' + config.db.user + ':' + config.db.password + '@' + config.db.host + '/' + config.db.name;
 
   return MongoClient.connect(url).then(function(conn){
       console.log('It is connected to db: ' + config.db.name);
