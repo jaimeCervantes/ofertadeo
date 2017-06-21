6fd4f74c60a591448fdc1d4a9de0e8c5e80de27f
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-06-20 21:04:39 -0500

    feat(js): adding morgan package for HTTP request logs

--- server/index.js
+++ server/index.js
@@ -6,23 +6,39 @@ import csm from './utils/sitemaps/create-sitemap.js'
 import helmet from 'helmet'
 import api from './api'
 import bodyParser from 'body-parser' 
+import morgan from 'morgan'
+import rfs from 'rotating-file-stream'
+import path from 'path'
+import fs from 'fs'
 
 const develop = !(process.env.NODE_ENV === 'production')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
+let morganFormat = 'combined'
+
+let logDirectory = path.join(__dirname, '../log')
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+// create a rotating write stream
+var accessLogStream = rfs('access.log', {
+  interval: '1d', // rotate daily
+  path: logDirectory
+})
 
 app.set('port', port)
 
 //For some reason in develop, express precondition provoke an error when the user load more
 //offers using ajax
 if(!develop) {
-	//var preconditions = require('express-preconditions');
+	//const preconditions = require('express-preconditions');
 	//app.use(preconditions())
+  //morganFormat = 'dev'
 }
 
 // Import API Routes
 app.use('/api', api)
+app.use(morgan(morganFormat, { stream: accessLogStream }))
 //Security
 app.use(helmet())
 app.disable('x-powered-by');
