1daac47d9a23460c41b5200aeef3d279fd9062a6
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-06-28 13:15:46 -0500

    Changing date format for sitemps

--- server/utils/sitemaps/utils.js
+++ server/utils/sitemaps/utils.js
@@ -10,7 +10,7 @@ function getDate () {
   var date = new Date();
   var substract = date.getTime() - (300*60*1000);
   var dateStr = new Date(substract).toISOString().split('.')[0];
-  return dateStr + '-05:00:00';
+  return dateStr + '-05:00';
 }
 
 function getData (params) {
@@ -59,7 +59,7 @@ function addToSitemap(sitemap, data, params) {
       url: params.route + '/' + current.slug,
       changefreq: params.changefreq || 'daily',
       priority: params.priority || 0.5,
-      lastmodISO: current.modified || getDate();
+      lastmodISO: current.modified || getDate()
     });
   });
 
