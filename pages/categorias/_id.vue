abcf719a558cd1dbe36f285605fc8a0a0b899d1a
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-07-21 15:11:45 -0500

    fixing visual string for items, stores and categories

--- pages/categorias/_id.vue
+++ pages/categorias/_id.vue
@@ -15,7 +15,7 @@
       </ofer-header-info>
     </template>
     <template slot="content" v-if="exists(info)">
-      <h2>Ofertas de {{info.name}}</h2>
+      <h2>Ofertas de {{info.name}} {{date}}</h2>
       <v-row>
         <v-col class="mt-3 mb-3" xs6 sm3 md3 lg2 xl2 v-for="(item,i) in items" :key="i">
           <ofer-item :item="item" :to-link="$store.state.routes.stores + '/' + item.slug"></ofer-item>
