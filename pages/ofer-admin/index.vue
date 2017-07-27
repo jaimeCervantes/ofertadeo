9c9af3bf013a3359515c949a9ddfe92eec9578d2
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-07-27 11:33:39 -0500

    Adding new changes to admin panel

--- pages/ofer-admin/index.vue
+++ pages/ofer-admin/index.vue
@@ -1,11 +1,11 @@
 <template>
   <ofer-content>
     <template slot="info-section">
-      <h1 class="title">Ofertas, promociones y descuentos en México</h1>
+      <h1>Ofertas, promociones y descuentos en México</h1>
       <p>Descubre las mejores ofertas, promociones y descuentos en México de las principales tiendas de tu ciudad como Walmart, Chedraui, Sams Club, Liverpool, Soriana, Cinepolis, Cinemex y muchas tiendas mas.  ✓ ¡Ahorra con ofertadeo!</p>
-      <h2 class="title">Lista de ofertas y promociones en México</h2>
     </template>
     <template slot="content">
+      <h2>Lista de ofertas y promociones en México</h2>
       <v-row>
         <v-col class="mt-3 mb-3" xs6 sm3 md3 lg2 xl2 v-for="(item,i) in items" :key="i">
           <ofer-item :item="item" :to-link="$store.state.routes.stores + '/' + item.slug"></ofer-item>
@@ -21,14 +21,12 @@
 <script>
 import axios from '~plugins/axios'
 import OferContent from '~components/ofer-content.vue'
-import OferCommon from '~components/mixins/ofer-common.vue'
 import OferItem from '~components/ofer-item.vue'
 import OferMoreItems from '~components/ofer-more-items.vue'
 
 var urlReq = '/api/home'
 
 export default {
-  mixins: [OferCommon],
   data () {
     return { urlReq: urlReq }
   },
@@ -39,15 +37,61 @@ export default {
   components: {
     OferContent,
     OferItem,
-    OferMoreItems,
-    OferCommon
+    OferMoreItems
+  },
+  methods: {
+    concatItems (items) {
+      this.items = this.items.concat(items)
+    }
   },
   head () {
+    let host = this.$store.state.host
+    let urlLogo = `${host}/logo.png`
     return {
-      title: 'Admin panel',
       meta: [
+        { hid: 'title', name: 'title', content: 'Ofertas, promociones y descuentos en México | Ofertadeo' },
+        { hid: 'publisher', rel: 'publisher', href: 'https://plus.google.com/110009950298317863923' },
+        { hid: 'description', name: 'description', content: 'Descubre las mejores ofertas, promociones y descuentos en México de las principales tiendas de tu ciudad.  ✓ ¡Ahorra con ofertadeo!' },
+        { hid: 'og:locale', property: 'og:locale', content: 'es_MX' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'Ofertadeo' },
+        { hid: 'og:title', property: 'og:title', content: 'Ofertas, promociones y descuentos en México | Ofertadeo' },
+        { hid: 'og:description', property: 'og:description', content: 'Descubre las mejores ofertas, promociones y descuentos en México de las principales tiendas de tu ciudad.  ✓ ¡Ahorra con ofertadeo!' },
+        { hid: 'og:url', property: 'og:url', content: host },
+        { hid: 'og:image', property: 'og:image', content: urlLogo },
+        { hid: 'og:image:secure_url', property: 'og:image:secure_url', content: urlLogo }
       ],
       link: [
+        { rel: 'canonical', href: host }
+      ],
+      script: [
+        {
+          innerHTML: JSON.stringify(
+            {
+              '@context': 'http://schema.org',
+              '@type': 'WebSite',
+              'name': 'Ofertadeo',
+              'url': host
+            }),
+          type: 'application/ld+json'
+        },
+        {
+          innerHTML: JSON.stringify(
+            {
+              '@context': 'http://schema.org',
+              '@type': 'Organization',
+              'url': host,
+              'logo': urlLogo,
+              'sameAs': [
+                'https://www.facebook.com/ofertadeo',
+                'https://twitter.com/ofertadeo',
+                'https://plus.google.com/110009950298317863923',
+                'https://www.youtube.com/channel/UCG8yl6wupq3kHem9SE4qa0g',
+                'https://es.pinterest.com/ofertadeo/'
+              ]
+            }),
+          type: 'application/ld+json'
+        }
       ]
     }
   }
