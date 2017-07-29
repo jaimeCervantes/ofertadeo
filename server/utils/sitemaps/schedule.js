76b71128f97bbac265cd0188906cfafd6513176b
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-07-28 21:53:58 -0500

    building for deploy

--- server/utils/sitemaps/schedule.js
+++ server/utils/sitemaps/schedule.js
@@ -1,18 +1,59 @@
 var cron = require('node-cron');
 var csm = require('./create-sitemap.js');
+var utils = require('./utils.js');
+var config = require('../../config.js')();
 
 
 var schedule = {
 	ping: ping
 };
 
+function checkDate () {
+	return utils.getData({
+		collecttion: config.db.collections.main,
+		query: {},
+		items_per_page: 1,
+		sort: { _id: -1 },
+		projection: { modified: 1 }
+	}).then(function(doc){
+		let lastOfferDate = new Date(doc.modified);
+		let lastOfferDateTime = new Date(doc.modified).getTime();
+		let hours = 12*60*60*1000;
+		let dateMinus12hrs = new Date().getTime() - hours;
+		if(lastOfferDateTime >= dateMinus12hrs) {
+			return {
+				lastOffer: new Date(doc.modified),
+				ping: true
+			};
+		}
+		return false;
+	});
+}
+
+
+
 function ping () {
-	cron.schedule('* 12 * * *', function (){//run every 5 minutes after midnigh everyday
-	  csm.ping();
+	//cron.schedule('* 12 * * *', function (){//run every 5 minutes after midnigh everyday
+	cron.schedule('*/1 * * * *', function (){//run every 5 minutes after midnigh everyday
+	  checkDate().then(function(res) {
+	  	if(res && res.ping) {
+	  		console.log(res)
+	  		csm.ping()
+	  	}
+	  }).catch(function(err) {
+	  	console.log(err);
+	  });
 	});
 
 	cron.schedule('50 23 * * *', function () {//run every 6 hours
-	  csm.ping();
+			checkDate().then(function(res) {
+		  	if(res && res.ping) {
+		  		console.log(res)
+		  		csm.ping()
+		  	}
+		  }).catch(function(err) {
+		  	console.log(err);
+		  });
 	});	
 }
 
