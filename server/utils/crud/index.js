6d23fec7bbd2c956f5f098a16d955b2137cc8e6a
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-08-04 02:01:36 -0500

    fix issue about wrong thumbnails

--- server/utils/crud/index.js
+++ server/utils/crud/index.js
@@ -3,19 +3,11 @@ const config = require('../../config.js');
 let conf = config();
 
 utils.update({
-	collections: ['offers', 'stores', 'categories', 'pages'],
-	query: { thumbnail: { $regex: /^[^(http)]/ } },
-	projection: { thumbnail: 1},
+	collections: ['categories', 'stores'],
+	query: {},
+	projection: { name: 1, img: 1 },
 	callback: function (conn, coll, doc) {
-		conn.collection(coll).updateOne(this.query, { $set: { thumbnail: conf.host + doc.thumbnail } })
-	}
-});
-
-utils.update({
-	collections: ['offers', 'stores', 'categories', 'pages'],
-	query: { img: { $regex: /^[^(http)]/ } },
-	projection: { img: 1 },
-	callback: function (conn, coll, doc) {
-		conn.collection(coll).updateOne(this.query, { $set: { img: conf.host + doc.img } })
+		var imgParts = doc.img.split('.');
+		conn.collection(coll).updateOne({id: doc._id}, { $set: { thumbnail: imgParts[0] + '_thumb.' + imgParts[1] } })
 	}
 });
