afd30dfb8c0c011a0932ad67d865246766a4db89
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-08-11 15:50:58 -0500

    building for deploy-develop

--- build/main.js
+++ build/main.js
@@ -65,7 +65,7 @@ module.exports =
 /******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 32);
+/******/ 	return __webpack_require__(__webpack_require__.s = 31);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -197,7 +197,7 @@ var config = {
     shard3: 'pensemosweb-shard-00-02-147ev.mongodb.net:27017',
     queryString: '?ssl=true&replicaSet=pensemosweb-shard-0&authSource=admin',
     mainCollection: 'offers',
-    itemsPerPage: 12,
+    itemsPerPage: 18,
     collections: {
       main: 'offers',
       secundary: 'stores',
@@ -257,7 +257,7 @@ module.exports = require("wagner-core");
 "use strict";
 
 
-var MongoClient = __webpack_require__(27).MongoClient;
+var MongoClient = __webpack_require__(26).MongoClient;
 
 function getConnection(config) {
   //connPromise is pending when trying to connect to mongodb atlas
@@ -291,7 +291,7 @@ var utils = __webpack_require__(8);
 var config = __webpack_require__(2)();
 var sm = __webpack_require__(9);
 var fs = __webpack_require__(3);
-var request = __webpack_require__(30);
+var request = __webpack_require__(29);
 
 config.paths.static = '/home/jaime/xml';
 var offers = '/sitemap-ofertas.xml';
@@ -412,11 +412,17 @@ var config = __webpack_require__(2)(wagner);
 __webpack_require__(6)(wagner);
 var CRUD = __webpack_require__(1);
 var fs = __webpack_require__(3);
-var zlib = __webpack_require__(31);
+var zlib = __webpack_require__(30);
 
-function getDate() {
-  var date = new Date();
-  var substract = date.getTime() - 300 * 60 * 1000;
+function getDate(dateObj) {
+  var date;
+  if (dateObj) {
+    date = new Date(dateObj);
+  } else {
+    date = new Date();
+  }
+
+  var substract = date.getTime() - 300 * 60 * 1000; // minus 5 hrs
   var dateStr = new Date(substract).toISOString().split('.')[0];
   return dateStr + '-05:00';
 }
@@ -463,7 +469,7 @@ function addToSitemap(sitemap, data, params) {
       url: params.route + '/' + current.slug,
       changefreq: params.changefreq || 'daily',
       priority: params.priority || 0.5,
-      lastmodISO: current.modified || getDate()
+      lastmodISO: getDate(current.modified)
     });
   });
 
@@ -574,7 +580,7 @@ router.use(upload(wagner));
 /* 12 */
 /***/ function(module, exports, __webpack_require__) {
 
-var cron = __webpack_require__(29);
+var cron = __webpack_require__(28);
 var csm = __webpack_require__(7);
 var utils = __webpack_require__(8);
 var config = __webpack_require__(2)();
@@ -588,7 +594,7 @@ function checkDate() {
 		collecttion: config.db.collections.main,
 		query: {},
 		items_per_page: 1,
-		sort: { _id: -1 },
+		sort: { modified: -1 },
 		projection: { modified: 1 }
 	}).then(function (doc) {
 		var lastOfferDate = new Date(doc.modified);
@@ -598,9 +604,9 @@ function checkDate() {
 		var minus12hrsDate = new Date(minus12hrsDateTime);
 		var resp = {
 			lastOffer: doc.modified,
-			lastOfferDate: lastOfferDate,
+			lastOfferDate: utils.getDate(lastOfferDate),
 			lastOfferDateTime: lastOfferDateTime,
-			minus12hrsDate: minus12hrsDate,
+			minus12hrsDate: utils.getDate(minus12hrsDate),
 			minus12hrsDateTime: minus12hrsDateTime,
 			ping: false
 		};
@@ -615,6 +621,7 @@ function checkDate() {
 }
 
 function ping() {
+	//cron.schedule('*/1 * * * *', function (){
 	cron.schedule('1 12 * * *', function () {
 		//run every day at 12:00 hrs
 		checkDate().then(function (res) {
@@ -627,6 +634,7 @@ function ping() {
 		});
 	});
 
+	//cron.schedule('*/1 * * * *', function (){
 	cron.schedule('50 23 * * *', function () {
 		//run every day at 23:50 hours
 		checkDate().then(function (res) {
@@ -723,7 +731,7 @@ function _id() {
       query: { categories: req.params._id },
       items_per_page: conf.db.itemsPerPage,
       skip: conf.db.itemsPerPage * page,
-      sort: { _id: -1 },
+      sort: { published: -1 },
       projection: {
         name: 1,
         thumbnail: 1,
@@ -833,7 +841,7 @@ function index() {
       collection: conf.db.collections.main,
       items_per_page: conf.db.itemsPerPage,
       skip: conf.db.itemsPerPage * page,
-      sort: { _id: -1 },
+      sort: { published: -1 },
       projection: {
         name: 1,
         thumbnail: 1,
@@ -871,7 +879,6 @@ function index() {
 
 var express = __webpack_require__(0);
 var router = express.Router();
-var utils = __webpack_require__(24);
 var CRUD = __webpack_require__(1);
 var csm = __webpack_require__(7);
 
@@ -944,10 +951,10 @@ function getFormDataPromotions() {
 
 function createPromotion() {
   router.post('/promotions/new', function (req, res) {
-    var rightNow = utils.getDate();
+    var rightNow = new Date();
     var data = Object.assign({ modified: rightNow, published: rightNow }, req.body);
     crudInst.setItem({
-      collection: conf.db.mainCollection,
+      collection: conf.db.collections.main,
       document: data
     }).then(function (result) {
       res.json(result);
@@ -955,17 +962,13 @@ function createPromotion() {
         // @TODO, when we use more than one category and more than one store, the updateOne 
         // operation should be execute for each element in the arrary categories and stores
         return Promise.all([crudInst.updateOne({
-          collection: conf.db.collections.main,
-          query: { _id: result.insertedId },
-          update: { $set: { modified: utils.getDate() } }
-        }), crudInst.updateOne({
           collection: conf.db.collections.secundary,
           query: { _id: data.stores[0] },
-          update: { $set: { modified: utils.getDate() } }
+          update: { $set: { modified: rightNow } }
         }), crudInst.updateOne({
           collection: conf.db.collections.categories,
           query: { _id: data.categories[0] },
-          update: { $set: { modified: utils.getDate() } }
+          update: { $set: { modified: rightNow } }
         })]);
       }
     }).then(function (results) {
@@ -1022,7 +1025,7 @@ function _id() {
       query: { stores: req.params._id },
       items_per_page: conf.db.itemsPerPage,
       skip: conf.db.itemsPerPage * page,
-      sort: { _id: -1 },
+      sort: { published: -1 },
       projection: {
         name: 1,
         thumbnail: 1,
@@ -1098,21 +1101,21 @@ function index() {
 
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_multer__ = __webpack_require__(28);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_multer__ = __webpack_require__(27);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_multer___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_multer__);
 
 
 var express = __webpack_require__(0);
 var router = express.Router();
 var CRUD = __webpack_require__(1);
-var mkdirp = __webpack_require__(26);
+var mkdirp = __webpack_require__(25);
 var path = __webpack_require__(4);
 
-var jimp = __webpack_require__(25);
+var jimp = __webpack_require__(24);
 
 var crudInst;
 var conf;
-var rootPathUploads = '/home/jaime';
+var rootPathUploads = '/home/jaime/static';
 module.exports = function (wagner, params) {
   wagner.invoke(function (conn, config) {
     conf = config;
@@ -1202,61 +1205,46 @@ function mkdirPromise(pathFile) {
 /* 24 */
 /***/ function(module, exports) {
 
-function getDate() {
-  var date = new Date();
-  var substract = date.getTime() - 300 * 60 * 1000;
-  var dateStr = new Date(substract).toISOString().split('.')[0];
-  return dateStr + '-05:00';
-}
-
-module.exports = {
-  getDate: getDate
-};
+module.exports = require("jimp");
 
 /***/ },
 /* 25 */
 /***/ function(module, exports) {
 
-module.exports = require("jimp");
+module.exports = require("mkdirp");
 
 /***/ },
 /* 26 */
 /***/ function(module, exports) {
 
-module.exports = require("mkdirp");
+module.exports = require("mongodb");
 
 /***/ },
 /* 27 */
 /***/ function(module, exports) {
 
-module.exports = require("mongodb");
+module.exports = require("multer");
 
 /***/ },
 /* 28 */
 /***/ function(module, exports) {
 
-module.exports = require("multer");
+module.exports = require("node-cron");
 
 /***/ },
 /* 29 */
 /***/ function(module, exports) {
 
-module.exports = require("node-cron");
+module.exports = require("request");
 
 /***/ },
 /* 30 */
 /***/ function(module, exports) {
 
-module.exports = require("request");
-
-/***/ },
-/* 31 */
-/***/ function(module, exports) {
-
 module.exports = require("zlib");
 
 /***/ },
-/* 32 */
+/* 31 */
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
