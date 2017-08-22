08ccf3b736536abcb4e34b33197e36378f13d045
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-08-22 12:32:55 -0500

    building for deploy-develop

--- build/main.js
+++ build/main.js
@@ -567,10 +567,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  // plugins: [
-  //   { src: '~plugins/ga.js', ssr: false },
-  //   { src: '~plugins/pc.js', ssr: false }
-  // ],
+  plugins: [{ src: '~plugins/ga.js', ssr: false }],
   /*
   ** Add axios globally
   */
