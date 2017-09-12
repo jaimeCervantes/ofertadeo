6cec5f7583f3682fe1fd0a6a6000b01a2c902b5d
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-09-12 09:33:59 -0500

    refactor(all): ofer-container, ofer-seo componets and store page

--- pages/tiendas/_id.vue
+++ pages/tiendas/_id.vue
@@ -1,5 +1,5 @@
 <template>
-  <ofer-content :breadcrumbs="exists(info) ? breadcrumbs : null">
+  <ofer-container :breadcrumbs="exists(info) ? breadcrumbs : null">
     <template slot="info-section" v-if="exists(info)">
       <ofer-header-info :info="info" rel="nofollow noopener">
         <template slot="social-network">
@@ -10,29 +10,34 @@
         </template>
       </ofer-header-info>
     </template>
-    <template slot="content-header">
-      <div class="info-content" v-html="info.content"></div>
-    </template>
     <template slot="content">
-      <h2 v-if="exists(info)" v-html="seo.h2"></h2>
-      <v-row v-if="exists(info)" id="main-list" itemscope itemtype="http://schema.org/ItemList">
-        <v-col class="mt-3 mb-3" xs6 sm3 md3 lg2 xl2 v-for="(item,i) in items" :key="i">
-          <ofer-item :item="item" type="store" :to-link="config.routes.main + '/' + item.slug" itemprop="itemListElement" itemscope itemtype="http://schema.org/Article" :position="i"></ofer-item>
-        </v-col>
-      </v-row>
+      <div v-if="exists(info)">
+        <div class="info-content"  v-html="info.content"></div>
+        <v-divider class="section-divider" v-if="exists(info)"></v-divider>
+      </div>
+      <div class="middle-content" v-if="exists(info)">
+        <h2 v-html="seo.h2"></h2>
+        <v-row v-if="exists(info)" id="main-list" itemscope itemtype="http://schema.org/ItemList">
+          <v-col class="mt-3 mb-3" xs6 sm3 md3 lg2 xl2 v-for="(item,i) in items" :key="i">
+            <ofer-item :item="item" type="store" :to-link="config.routes.main + '/' + item.slug" itemprop="itemListElement" itemscope itemtype="http://schema.org/Article" :position="i"></ofer-item>
+          </v-col>
+        </v-row>
+      </div>
+      <div v-if="exists(info)">
+        <ofer-more-items @more-items="concatItems" :pagination="pagination" :url="urlReq+id" txt="Cargar más ofertas"></ofer-more-items>
+      </div>
       <ofer-not-exists v-if="!exists(info)" v-bind:title="notExistTitle"></ofer-not-exists>
     </template>
-    <template slot="more-content" v-if="exists(info)">
-      <ofer-more-items @more-items="concatItems" :pagination="pagination" :url="urlReq+id" txt="Cargar más ofertas"></ofer-more-items>
-    </template>
-  </ofer-content>
+  </ofer-container>
 </template>
 
 <script>
 import axios from '~plugins/axios'
 import OferContent from '~components/ofer-content.vue'
+import OferContainer from '~components/ofer-container.vue'
 import OferHeaderInfo from '~components/ofer-header-info.vue'
 import OferCommon from '~components/mixins/ofer-common.vue'
+import OferSeo from '~components/mixins/ofer-seo.vue'
 import OferPaths from '~components/mixins/ofer-paths.vue'
 import OferItem from '~components/ofer-item.vue'
 import OferMoreItems from '~components/ofer-more-items.vue'
@@ -43,7 +48,7 @@ import ShareButtons from '~components/share-buttons.vue'
 var urlReq = '/api/stores/'
 
 export default {
-  mixins: [OferPaths, OferCommon],
+  mixins: [OferPaths, OferCommon, OferSeo],
   data () {
     return {
       urlReq: urlReq,
@@ -53,23 +58,27 @@ export default {
   },
   async asyncData ({ params, route }) {
     let { data } = await axios.get(urlReq + params.id)
-    if (data && data.info) {
-      // data.info.description = `Descubre las mejores ofertas y promociones de ${data.info.name} en Ofertadeo. Descuentos, promociones y ofertas en ${data.info.name} ${OferCommon.methods.getDate('YYYY')}. ❤ ¡Ahorra ya!`
-      data.seo = OferCommon.methods.getSEOData(data.seo, data.info)
-      data.info.h1 = data.seo.h1
-      data.info.description = data.seo.description
-      data.info.img_alt = `Ofertas ${data.info.name}`
-    }
-
     return Object.assign({ path: route.path, id: params.id }, data)
+  },
+  created () {
+    // data.info.description = `Descubre las mejores ofertas y promociones de ${data.info.name} en Ofertadeo. Descuentos, promociones y ofertas en ${data.info.name} ${OferCommon.methods.getDate('YYYY')}. ❤ ¡Ahorra ya!`
+    if (this.exists(this.seo) && this.exists(this.info)) {
+      this.seo = this.getSeoData(this.seo, this.info)
+      this.seo = this.getDefaultsForSeo(this.seo, this.info)
+      this.info.h1 = this.seo.h1
+      this.info.description = this.seo.description
+      this.info.img_alt = `Ofertas ${this.info.name}`
+    }
   },
   components: {
     OferContent,
+    OferContainer,
     OferItem,
     OferMoreItems,
     OferHeaderInfo,
     OferNotExists,
     OferCommon,
+    OferSeo,
     ShareButtons
   },
   head () {
