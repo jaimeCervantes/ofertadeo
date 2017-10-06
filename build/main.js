00119a203bbed67b6944e1e1e827af365c77ef4d
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-10-06 10:51:41 -0500

    building for deploy-develop

--- build/main.js
+++ build/main.js
@@ -92,6 +92,9 @@ var config = {
       categories: 'categories',
       pages: 'pages',
       seo: 'seo'
+    },
+    collation: {
+      locale: 'es'
     }
   },
   routes: {
@@ -339,9 +342,9 @@ module.exports = function crud(spec, shared) {
     });
   };
 
-  var aggregation = function aggregation(params) {
+  var aggregate = function aggregate(params) {
     var db = params.db || spec.DATABASE;
-    return db.collection(params.collection || spec.COLLECTION).aggregate(params.aggregation).toArray().then(function (data) {
+    return db.collection(params.collection || spec.COLLECTION).aggregate(params.pipeline, params.options).toArray().then(function (data) {
       return data;
     }).catch(function (err) {
       return err;
@@ -355,7 +358,7 @@ module.exports = function crud(spec, shared) {
   that.update = update;
   that.searchItems = searchItems;
   that.getPagination = getPagination;
-  that.aggregation = aggregation;
+  that.aggregate = aggregate;
 
   return that;
 };
@@ -1266,30 +1269,39 @@ module.exports = function (spec) {
 
     spec.router.get('/stores', function (req, res) {
       var page = req.query.page ? Number(req.query.page) : 0;
-      var iterable = [crudInst.getItems({
+      var iterable = [crudInst.aggregate({
         collection: conf.db.collections.secundary,
-        items_per_page: conf.db.itemsPerPage,
-        skip: conf.db.itemsPerPage * page,
-        sort: { name: 1 },
-        projection: {
-          name: 1,
-          slug: 1,
-          thumbnail: 1,
-          img: 1,
-          img_data: 1,
-          img_alt: 1,
-          img_title: 1,
-          published: 1,
-          modified: 1
-        }
-      }), crudInst.getPagination({
-        collection: conf.db.collections.secundary
-      })];
+        pipeline: [{ $limit: 100 }, // Primiero limitar la cantidad
+        // Ordenarlo por nombre para obtener el arreglo stores en el sig, pipe ordenado alfabeticamente en orden ascendente
+        { $sort: { name: 1 } }, { $project: {
+            // A mayusculas, porque 'e' !== 'E'
+            _id: { $toUpper: "$_id" },
+            name: 1,
+            slug: 1,
+            thumbnail: 1,
+            img: 1,
+            img_data: 1,
+            img_alt: 1,
+            img_title: 1,
+            published: 1,
+            modified: 1
+          }
+        }, { $group: {
+            _id: { $substrCP: ["$_id", 0, 1] },
+            stores: { $push: "$$CURRENT" }
+          }
+        }, { $sort: { _id: 1 } }], // pipeline
+        options: {
+          collation: {
+            locale: conf.db.collation.locale
+          } // options
+        } }) // aggregate
+      ]; // iterable
 
       Promise.all(iterable).then(function (results) {
+        console.log(results[0]);
         res.json({
-          items: results[0],
-          pagination: results[1]
+          items: results[0]
         });
       }).catch(function (error) {
         console.log(error);
