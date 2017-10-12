5fbb3a88fd2fbf522bca3910b448fc52ce9e78f7
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-10-12 06:31:13 -0500

    fix(js): backend, saving published and modified dates as date instead of string

--- server/api/handlers/categories.js
+++ server/api/handlers/categories.js
@@ -184,11 +184,14 @@ module.exports = function (spec) {
       let saveParams = {
         collection: conf.db.collections.categories
       }
-      let data = Object.assign({ modified: rightNow }, req.body)
+      let data = Object.assign(req.body, { modified: rightNow })
 
       // Al crear nueva, la fecha de publicacion y modificacion son la misma
       if (!data.hasOwnProperty('published')) {
         data.published = rightNow
+      } else {
+        //asegurandonos que se guarde como campo tipo fecha
+        data.published = new Date(data.published)
       }
 
       if (data.hasOwnProperty('_id')) {
