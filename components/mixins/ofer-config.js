137e4080d329a6f5c79514b00a4df328b6306182
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-10-24 22:28:58 -0500

    feat(all): Adding admin menu and missing admin views

--- components/mixins/ofer-config.js
+++ components/mixins/ofer-config.js
@@ -24,13 +24,16 @@ export default {
     main: '/promociones',
     storeList: '/tiendas',
     admin: '/ofer-admin',
+    seo: '/seo',
     elBuenFin: '/el-buen-fin'
   },
   host: 'http://localhost:3000',
   urlLogo: 'http://localhost:3000/logo.png',
   txt: {
     categories: 'Categorías',
-    stores: 'Tiendas'
+    stores: 'Tiendas',
+    offers: 'Promociones',
+    elBuenFin: 'El Buen Fin'
   },
   publisher: {
     fb: 'https://www.facebook.com/ofertadeo',
--- pages/ofer-admin/categorias/editar/index.vue
+++ pages/ofer-admin/categorias/editar/index.vue
@@ -1,33 +1,33 @@
 <template>
   <ofer-content :breadcrumbs="breadcrumbs">
     <template slot="info-section">
-      <h1>Ofertas, promociones y descuentos en Tiendas de México</h1>
-      <h2 class="subheading">En esta sección encontrarás las mejores ofertas, promociones y descuentos de las principales tiendas de México ordenas alfabéticamente.</h2>
+      <h1 class="title">Ofertas, promociones y descuentos por Categorías.</h1>
+      <h2 class="subheading">En esta sección encontrarás las mejores ofertas, promociones y descuentos de Categorías como Frutas y Verduras, Meses sin Intereses, Ventas Nocturnas y más ordenadas alfabéticamente.</h2>
     </template>
     <template slot="content">
       <div id="main-list" itemscope itemtype="http://schema.org/ItemList">
-        <div v-for="(letter,l) in items" :key="l">
+        <div v-for="(letter, j) in items" :key="j">
           <h3 v-text="letter._id.toUpperCase()"></h3>
           <v-row >
-            <v-col class="mt-3 mb-3" xs6 sm3 md3 lg2 xl2 v-for="(item,i) in letter.stores" :key="i">
-              <ofer-item :item="item" :to-link="config.routes.storeList + '/' + item._id" itemprop="itemListElement" itemscope itemtype="http://schema.org/Article" :position="i">
+            <v-col class="mt-3 mb-3" xs6 sm3 md3 lg2 xl2 v-for="(item, i) in letter.categories" :key="i">
+              <ofer-item class="mini" :item="item" :to-link="config.routes.categories + '/' + item._id" itemprop="itemListElement" itemscope itemtype="http://schema.org/Article" :position="i">
                 <template slot="content">
                   <v-card-row class="item__name" v-if="item.name" itemprop="mainEntityOfPage">
                     <div class="pl-2 pr-2" itemprop="headline">
                       <a itemprop="name" :href="getItemLink(item._id)">
-                        {{sliceTextFromHtml(item.name, 45)}}
+                        {{item.name}}
                       </a>
                     </div>
                   </v-card-row> 
                   <v-card-row>
                     <div class="pl-2 pr-2">
-                      <v-btn small primary dark tag="a" :to="`${config.host}${config.routes.admin}${config.routes.storeList}/editar/${item.slug}`">Editar</v-btn>
+                      <v-btn small primary dark tag="a" :to="`${config.host}${config.routes.admin}${config.routes.categoriesList}/editar/${item.slug}`">Editar</v-btn>
                     </div>
                   </v-card-row>
                 </template>
               </ofer-item>
             </v-col>
-          </v-row>
+          </v-row>          
         </div>
       </div>
     </template>
@@ -41,7 +41,7 @@ import OferPaths from '~/components/mixins/ofer-paths.vue'
 import OferCommon from '~/components/mixins/ofer-common.vue'
 import OferItem from '~/components/ofer-item.vue'
 
-var urlReq = OferCommon.props.config.default().host + '/api/stores'
+var urlReq = OferCommon.props.config.default().host + '/api/categories'
 
 export default {
   layout: 'admin',
@@ -63,12 +63,12 @@ export default {
   },
   methods: {
     getItemLink (path) {
-      return `${this.config.host}${this.config.routes.storeList}/${path}`
+      return `${this.config.host}${this.config.routes.categoriesList}/${path}`
     }
   },
   head () {
     return {
-      title: 'Tiendas | Ofertadeo',
+      title: 'Editar Categorias | Ofertadeo',
       meta: [
         { hid: 'robots', name: 'robots', content: 'noindex, nofollow' }
       ]
@@ -76,3 +76,12 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+  h3 {
+    margin-bottom: 0;
+    padding-bottom: 0;
+    margin-top: 1.2rem;
+    font-size: 1.5rem;
+  }
+</style>
