334d0258d0e41563cfe400cad8c32661d3989cbb
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-11-06 22:21:05 -0600

    Not footer, building for deploy

--- pages/categorias/index.vue
+++ pages/categorias/index.vue
@@ -1,72 +1,64 @@
 <template>
-  <div>
-    <v-container grid-list-md fluid>
-      <header>
-        <v-breadcrumbs divider="/" v-if="breadcrumbs">
-          <v-breadcrumbs-item
-            v-for="item in breadcrumbs" :key="item.href"
-            :disabled="item.disabled"
-            :href="item.href"
-            :target="item.target || '_self'"
-          >
-            {{ item.text }}
-          </v-breadcrumbs-item>
-        </v-breadcrumbs>
-        <h1 class="title">Ofertas, promociones y descuentos por Categorías.</h1>
-        <h2 class="subheading">En esta sección encontrarás las mejores ofertas, promociones y descuentos de Categorías como Frutas y Verduras, Meses sin Intereses, Ventas Nocturnas y más ordenadas alfabéticamente.</h2>
-      </header>
-    </v-container>
-    <v-container fluid>
-      <v-layout column>
-          <!-- encabezado-adaptable -->
-          <ins class="adsbygoogle"
-               style="display:block"
-               data-ad-client="ca-pub-3434008864168200"
-               data-ad-slot="2635524670"
-               data-ad-format="auto"></ins>
-          <script>
-          (window.adsbygoogle || []).push({})
-          </script>
-      </v-layout>
-    </v-container>
-    <v-container grid-list-md fluid>
-      <section>
-        <div id="main-list" itemscope itemtype="http://schema.org/ItemList">
-          <div v-for="(letter, j) in items" :key="j">
-            <h3 v-text="letter._id.toUpperCase()"></h3>
-            <v-layout row wrap>
-              <v-flex xs6 sm3 md3 lg2 xl2 v-for="(item, i) in letter.categories" :key="i">
-                <ofer-item class="mini" :item="item" :to-link="config.routes.categories + '/' + item._id" itemprop="itemListElement" itemscope itemtype="http://schema.org/Article" :position="i">
-                  <template slot="content">
-                    <div class="item__name" v-if="item.name" itemprop="mainEntityOfPage">
-                      <div class="pl-2 pr-2" itemprop="headline">
-                        <a itemprop="name" :href="getItemLink(item._id)">
-                          {{item.name}}
-                        </a>
-                      </div>
-                    </div> 
-                  </template>
-                </ofer-item>
-              </v-flex>
-            </v-layout>          
-          </div>
+  <v-container grid-list-md fluid>
+    <header>
+      <v-breadcrumbs divider="/" v-if="breadcrumbs">
+        <v-breadcrumbs-item
+          v-for="item in breadcrumbs" :key="item.href"
+          :disabled="item.disabled"
+          :href="item.href"
+          :target="item.target || '_self'"
+        >
+          {{ item.text }}
+        </v-breadcrumbs-item>
+      </v-breadcrumbs>
+      <h1 class="title">Ofertas, promociones y descuentos por Categorías.</h1>
+      <h2 class="subheading">En esta sección encontrarás las mejores ofertas, promociones y descuentos de Categorías como Frutas y Verduras, Meses sin Intereses, Ventas Nocturnas y más ordenadas alfabéticamente.</h2>
+    </header>
+    <div class="anuncio">
+      <!-- encabezado-adaptable -->
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-3434008864168200"
+           data-ad-slot="2635524670"
+           data-ad-format="auto"></ins>
+      <script>
+      (window.adsbygoogle || []).push({})
+      </script>
+    </div>
+    <section>
+      <div id="main-list" itemscope itemtype="http://schema.org/ItemList">
+        <div v-for="(letter, j) in items" :key="j">
+          <h3 v-text="letter._id.toUpperCase()"></h3>
+          <v-layout row wrap>
+            <v-flex xs6 sm3 md3 lg2 xl2 v-for="(item, i) in letter.categories" :key="i">
+              <ofer-item class="mini" :item="item" :to-link="config.routes.categories + '/' + item._id" itemprop="itemListElement" itemscope itemtype="http://schema.org/Article" :position="i">
+                <template slot="content">
+                  <div class="item__name" v-if="item.name" itemprop="mainEntityOfPage">
+                    <div class="pl-2 pr-2" itemprop="headline">
+                      <a itemprop="name" :href="getItemLink(item._id)">
+                        {{item.name}}
+                      </a>
+                    </div>
+                  </div> 
+                </template>
+              </ofer-item>
+            </v-flex>
+          </v-layout>          
         </div>
-      </section>
-    </v-container>
-    <v-container fluid>
-      <v-layout column>
-          <!-- pie-adaptable -->
-          <ins class="adsbygoogle"
-               style="display:block"
-               data-ad-client="ca-pub-3434008864168200"
-               data-ad-slot="8124489226"
-               data-ad-format="auto"></ins>
-          <script>
-          (window.adsbygoogle || []).push({})
-          </script>
-      </v-layout>
-    </v-container>
-  </div>
+      </div>
+    </section>
+    <div class="anuncio">
+      <!-- pie-adaptable -->
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-3434008864168200"
+           data-ad-slot="8124489226"
+           data-ad-format="auto"></ins>
+      <script>
+      (window.adsbygoogle || []).push({})
+      </script>
+    </div>
+  </v-container>
 </template>
 
 <script>
