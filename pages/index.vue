959265d1452b0151489653c68ae40176226eaf8a
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-11-06 19:19:17 -0600

    building for deploy

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,18 @@
     <header>
       <h1>Ofertas, promociones y descuentos en México</h1>
       <p>Descubre las mejores ofertas, promociones y descuentos en México de las principales tiendas de tu ciudad como Walmart, Chedraui, Sams Club, Liverpool, Soriana, Cinepolis, Cinemex y muchas tiendas mas.  ✓ ¡Ahorra con ofertadeo!</p>
-      <v-layout class="anuncio">
-        <v-flex xs12 sm12 md12 lg12 xl12>
-          <!-- encabezado-adaptable -->
-          <ins class="adsbygoogle"
-               style="display:block"
-               data-ad-client="ca-pub-3434008864168200"
-               data-ad-slot="2635524670"
-               data-ad-format="auto"></ins>
-          <script>
-          (window.adsbygoogle || []).push({})
-          </script>
-        </v-flex>
-      </v-layout >
     </header>
+    <div class="anuncio">
+      <!-- encabezado-adaptable -->
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-3434008864168200"
+           data-ad-slot="2635524670"
+           data-ad-format="auto"></ins>
+      <script>
+      (window.adsbygoogle || []).push({})
+      </script>
+    </div>
     <h2>Lista de ofertas y promociones en México</h2> 
     <v-layout row wrap id="main-list" itemscope itemtype="http://schema.org/ItemList">
       <link itemprop="url" :href="config.host" />
@@ -25,19 +23,18 @@
       </v-flex>
     </v-layout>
     <ofer-more-items @more-items="concatItems" :pagination="pagination" :url="urlReq" txt="Cargar más ofertas"></ofer-more-items>
-    <v-layout  class="anuncio">
-      <v-flex xs12 sm12 md12 lg12 xl12>
-        <!-- pie-adaptable -->
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-3434008864168200"
-             data-ad-slot="8124489226"
-             data-ad-format="auto"></ins>
-        <script>
-        (window.adsbygoogle || []).push({})
-        </script>
-      </v-flex>
     </v-layout>
+    <div class="anuncio">
+      <!-- pie-adaptable -->
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-3434008864168200"
+           data-ad-slot="8124489226"
+           data-ad-format="auto"></ins>
+      <script>
+      (window.adsbygoogle || []).push({})
+      </script>
+    </div>
   </v-container>
 </template>
 
